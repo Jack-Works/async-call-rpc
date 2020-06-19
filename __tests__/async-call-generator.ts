adfb8bb31c7d6968d22734e347a6e03d8ff06f2a
Jack Works <jackworks@protonmail.com>
2020-06-19 12:37:04 +0800

    test: add more test to async generator

--- __tests__/async-call-generator.ts
+++ __tests__/async-call-generator.ts
@@ -1,9 +1,51 @@
 import { createGeneratorServer } from './shared'
 
 test('Async Generator Call', async () => {
-    const server = createGeneratorServer()
+    const server = createGeneratorServer({ strict: true })
     let i = 0
     const number = [1, 2]
     for await (const x of server.gen(...number)) i += x
     expect(i).toBe(3)
+    // @ts-expect-error
+    await expect(server.gen2().next()).rejects.toMatchInlineSnapshot(`[Error: gen2 is not a function]`)
+    expect(() => server[Symbol.for('xyz')]).toThrow()
+})
+
+test('Async Generator Call manual', async () => {
+    const server = createGeneratorServer()
+    const iter = server.gen(1, 2)
+    // @ts-ignore
+    server.gen2(1, 2).next()
+    await expect(iter.next()).resolves.toMatchInlineSnapshot(`
+                Object {
+                  "done": false,
+                  "value": 1,
+                }
+            `)
+    await expect(iter.next()).resolves.toMatchInlineSnapshot(`
+                Object {
+                  "done": false,
+                  "value": 2,
+                }
+            `)
+    await expect(iter.next()).resolves.toMatchInlineSnapshot(`
+                Object {
+                  "done": true,
+                  "value": undefined,
+                }
+            `)
+    await expect(iter.next()).resolves.toMatchInlineSnapshot(`
+                Object {
+                  "done": true,
+                  "value": undefined,
+                }
+            `)
+    // @ts-ignore
+    await expect(iter.return(2)).resolves.toMatchInlineSnapshot(`
+                Object {
+                  "done": true,
+                  "value": 2,
+                }
+            `)
+    await expect(iter.throw()).rejects.toMatchInlineSnapshot(`[Error]`)
 })
