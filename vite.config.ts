8809c545b0ce0acaededec4c1779bc18ef915d05
Jack Works <jackworks@protonmail.com>
2024-02-11 17:48:48 +0800

    fix: test coverage

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
     test: {
         include: ['./__tests__/*.ts'],
+        coverage: {
+            provider: 'v8',
+        },
     },
 })
